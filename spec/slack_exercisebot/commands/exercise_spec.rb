d3d8807db36b8ce941fa5e5e0704f76a7d1ce83d
Sergio D Perdomo <sergio.931129@gmail.com>
2020-04-02 09:03:21 -0500

    Fixed Lint error

--- spec/slack_exercisebot/commands/exercise_spec.rb
+++ spec/slack_exercisebot/commands/exercise_spec.rb
@@ -10,8 +10,8 @@ describe SlackExercisebot::Commands::GetExercise do
   it 'returns exercise #1' do
     expect(
       message: "#{SlackRubyBot.config.user} Exercise1", channel: 'channel'
-      ).to respond_with_slack_message(
+    ).to respond_with_slack_message(
         'Lets start with High Knees. Do it for 20 seconds!!'
-        )
+      )
   end
 end
